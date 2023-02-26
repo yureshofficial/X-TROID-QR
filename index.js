9bd170f852be5e0012766ae70086efff2a9ac0e3
YURESH OFFICIAL <123641928+yureshofficial@users.noreply.github.com>
2023-02-26 13:23:00 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,47 @@
-const express = require('express')
-const PORT = process.env.PORT || 8080;
-var app = express();
-const fs = require('fs')
-var main = require('./main');
+let cluster = require('cluster')
+let path = require('path')
+let fs = require('fs')
+const Readline = require('readline')
 
-app.use('/', main);
-app.listen(PORT, () => {
-    console.log(`Server Run on port ${PORT}`)
-});
+var isRunning = false
+/**
+ * Start a js file
+ * @param {String} file `path/to/file`
+ */
+function start(file) {
+  if (isRunning) return
+  isRunning = true
+  let args = [path.join(__dirname, file), ...process.argv.slice(2)]
+
+  cluster.setupMaster({
+    exec: path.join(__dirname, file),
+    args: args.slice(1),
+  })
+  let p = cluster.fork()
+  p.on('message', data => {
+    console.log('[RECEIVED]', data)
+    switch (data) {
+      case 'reset':
+        p.kill()
+        isRunning = false
+        start.apply(this, arguments)
+        break
+      case 'uptime':
+        p.send(process.uptime())
+        break
+    }
+  })
+  p.on('exit', code => {
+    isRunning = false
+    console.error('Exited with code:', code)
+    if (code === 0) return
+    fs.watchFile(args[0], () => {
+      fs.unwatchFile(args[0])
+      start(file)
+    })
+  })
+
+}
+
+
+start('main.js')
